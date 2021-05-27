4ffdd9a6ba60c6f8a64007bcea650e0d836f1f5d
00589117 <00589117@PC008124154.cathaybk.intra.uwccb>
2021-05-27 21:57:29 +0800

    test02

--- src/main/java/com/example/demospringboot/DemospringbootApplication.java
+++ src/main/java/com/example/demospringboot/DemospringbootApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.demospringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemospringbootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(DemospringbootApplication.class, args);
 	}
 
 }
